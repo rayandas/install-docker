70b4df80e9b37aa2814f99906210366c3459c4b2
Rayan Das <rayandas91@gmail.com>
2022-07-29 13:00:57 +0530

    Add Docker 20.10.13

---
+++ pkg/20.10.13/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DOCKER_GIT_COMMIT="b2e29ef7a9a89840d2333637f7d1900a83e7153f"
+
+TEST_OS_IMAGE_NAME=(oraclelinux centos ubuntu rockylinux)
+TEST_OS_IMAGE_TAG[0]="7 8"
+TEST_OS_IMAGE_TAG[1]="centos7 centos8"
+TEST_OS_IMAGE_TAG[2]="16.04 18.04 20.04"
+TEST_OS_IMAGE_TAG[3]="8"
